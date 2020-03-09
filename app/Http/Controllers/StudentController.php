b0cac7432e177456b24661dd484f12fa3b6c818e
Partha Sarathi Acharjya <39263124+partha-uiu@users.noreply.github.com>
2020-03-09 16:40:25 +0600

    Student controller methods setup

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -6,5 +6,40 @@ use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
-    //
+    public function index()
+    {
+        
+    }
+
+    public function create()
+    {
+        return view('students.create');
+    }
+
+    public function store(Request $request)
+    {
+       
+    }
+
+    public function show()
+    {
+        
+    }
+
+
+    public function edit()
+    {
+
+    }
+
+    public function update(Request $request, Student $student)
+    {
+       
+    }
+
+
+    public function destroy(Student $student)
+    {
+       
+    }
 }
--- app/Student.php
+++ app/Student.php
@@ -6,41 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
-    public function index()
-    {
-        $students = Student::latest()->paginate(5);
-        return view('students.index',compact('students'))->with('i', (request()->input('page', 1) - 1) * 5);
-    }
-
-    public function create()
-    {
-        return view('students.create');
-    }
-
-    public function store(Request $request)
-    {
-       
-    }
-
-    public function show()
-    {
-        
-    }
-
-
-    public function edit()
-    {
-
-    }
-
-    public function update(Request $request, Student $student)
-    {
-       
-    }
-
-
-    public function destroy(Student $student)
-    {
-       
-    }
+  
 }
